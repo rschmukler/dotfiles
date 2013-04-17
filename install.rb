def949bc9ca737b5bd963f33952a7922db549fcc
Ryan Schmukler <ryan@womstreet.com>
2013-04-17 17:21:43 -0500

    Variable bug

--- install.rb
+++ install.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-home = File.expand_path('~')
+HOME = File.expand_path('~')
 
 
 def symlink_files
   Dir['*'].each do |file|
     next if file =~ /install/
-    target = File.join(home, ".#{file}")
+    target = File.join(HOME, ".#{file}")
     unless File.exists? target
       `ln -s #{File.expand_path file} #{target}`
     end
@@ -16,14 +16,14 @@ def symlink_files
   install_files = Dir['install/{.**,**}/**/*'].reject{ |x| x =~ /(\.\.|\.$)/ }.reject{ |x| File.directory?(x) }
   install_files.each do |f|
     install_path = f[8..-1]
-    target = File.join(home, install_path)
+    target = File.join(HOME, install_path)
     `mkdir -p #{File.dirname(target)}`
     `ln -s #{File.expand_path f} #{target}`
   end
 end
 
 def make_vim_tmp
-  vim_tmp = File.join(home, '.vim-tmp')
+  vim_tmp = File.join(HOME, '.vim-tmp')
   unless File.exists? vim_tmp
     `mkdir #{vim_tmp}`
   end
