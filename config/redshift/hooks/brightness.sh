44c5491f817fc1133e55a33a091ae46027da3c04
Ryan Schmukler <rschmukler@gmail.com>
2019-09-09 00:33:54 -0400

    feat: redshift config

---
+++ config/redshift/hooks/brightness.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Set brightness via xbrightness when redshift status changes
+
+# Set brightness values for each status.
+# Range from 1 to 100 is valid
+brightness_day=90
+brightness_transition=50
+brightness_night=10
+# Set fade time for changes to one minute
+fade_time=60000
+
+if [ "$1" = period-changed ]; then
+	case $3 in
+		night)
+			ddcutil --display=1 setvcp 10 $brightness_night
+			ddcutil --display=2 setvcp 10 $brightness_night
+			;;
+		transition)
+			ddcutil --display=1 setvcp 10 $brightness_transition
+			ddcutil --display=2 setvcp 10 $brightness_transition
+			;;
+		daytime)
+			xbacklight -set $brightness_day -time $fade_time
+			ddcutil --display=1 setvcp 10 $brightness_day
+			ddcutil --display=2 setvcp 10 $brightness_day
+			;;
+	esac
+fi
